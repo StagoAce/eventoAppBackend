648bd258acf631939ce2d918107b06c3578e28ac
StagoAce <99208572+StagoAce@users.noreply.github.com>
2024-10-29 22:53:00 -0500

    ALLOWED_HOSTS

--- eventoAppBackend/settings.py
+++ eventoAppBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&3f=*it2x_e1br=7ak3b2h(=50ij%+9miu2mq#irt%ri%5m)+9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
